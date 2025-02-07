5625712925bff2445b4296d8344b3902f3bc90a6
MajinDohko <agarridoluengo@gmail.com>
2025-02-07 11:14:19 +0100

    corregido error

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8000;
 require('dotenv').config();
 const {dbConnection} = require('./config/config');
-const router = require('./routes');
+const router = require('./routes/tasks');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
